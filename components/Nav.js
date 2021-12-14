a013bbab2d738206fdb2762f4cef691107656643
Chauncey Currie <javonnedadon@gmail.com>
2021-12-13 21:01:42 -0800

    updates

--- components/Nav.js
+++ components/Nav.js
@@ -1,13 +1,13 @@
 import html from "html-literal";
 
-export default (links) => html`
+export default links => html`
   <nav>
     <i class="fas fa-bars"></i>
     <ul class="hidden--mobile nav-links">
-      <li><a href="index.html">Home</a></li>
-      <li><a href="bio.html">Bio</a></li>
-      <li><a href="gallery.html">Gallery</a></li>
-      <li><a href="register.html">Register</a></li>
+      <li><a href="Home.js">Home</a></li>
+      <li><a href="Bio.js">Bio</a></li>
+      <li><a href="Gallery.js">Gallery</a></li>
+      <li><a href="Register.js">Register</a></li>
     </ul>
   </nav>
 `;
