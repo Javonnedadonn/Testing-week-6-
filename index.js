dba2a8aa12a4e3bbd096cefca2937de79e036b39
Chauncey Currie <javonnedadon@gmail.com>
2021-12-16 18:30:50 -0800

    recent updates

--- index.js
+++ index.js
@@ -1,19 +1,12 @@
 import { Header, Nav, Main, Footer } from "./components";
 import * as state from "./store";
+import axios from "axios";
 import Navigo from "navigo";
 import { capitalize } from "lodash";
 
 const router = new Navigo("/");
 
-router
-  .on({
-    "/": () => render(state.Home),
-    ":page": params => {
-      let page = capitalize(params.page);
-      render(state[page]);
-    }
-  })
-  .resolve();
+
 
 function render(st) {
   document.querySelector("#root").innerHTML = `
@@ -60,3 +53,50 @@ function addEventListeners(st) {
     });
   }
 }
+
+router.hooks({
+  before: (done, params) => {
+    const page =
+      params && params.hasOwnProperty("page")
+        ? capitalize(params.page)
+        : "Home";
+
+    if (page === "Home") {
+      axios
+        .get(
+          `https://api.openweathermap.org/data/2.5/weather?appid=fbb30b5d6cf8e164ed522e5082b49064&q=st.%20louis`
+        )
+        .then((response) => {
+          state.Home.weather = {};
+          state.Home.weather.city = response.data.name;
+          state.Home.weather.temp = response.data.main.temp;
+          state.Home.weather.feelsLike = response.data.main.feels_like;
+          state.Home.weather.description = response.data.weather[0].main;
+          done();
+        })
+        .catch((err) => console.log(err));
+    }
+
+    if (page === "Pizza") {
+      axios
+        .get(`${process.env.PIZZA_PLACE_API_URL}`)
+        .then((response) => {
+          state.Pizza.pizzas = response.data;
+          done();
+        })
+        .catch((error) => {
+          console.log("It puked", error);
+        });
+    }
+  },
+});
+
+router
+  .on({
+    "/": () => render(state.Home),
+    ":page": params => {
+      let page = capitalize(params.page);
+      render(state[page]);
+    }
+  })
+  .resolve();
--- store/Links.js
+++ store/Links.js
@@ -1,6 +1,7 @@
+/* eslint-disable prettier/prettier */
 export default [
   { title: "Home", text: "Home" },
   { title: "Gallery", text: "Gallery" },
   { title: "Bio", text: "Bio" },
-  { title: "Register", text: "Register" }
+  { title: "Pizza", text: "Pizza!" },
 ];
