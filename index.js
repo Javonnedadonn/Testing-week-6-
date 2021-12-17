218c33b721c5da8da8c816f66fdd139cc84656d6
Chauncey Currie <javonnedadon@gmail.com>
2021-12-16 20:56:18 -0800

    recent update

--- index.js
+++ index.js
@@ -67,7 +67,7 @@ router.hooks({
     if (page === "Home") {
       axios
         .get(
-          `https://api.openweathermap.org/data/2.5/weather?appid=fbb30b5d6cf8e164ed522e5082b49064&q=st.%20louis`
+          `https://api.openweathermap.org/data/2.5/weather?appid=${process.env.OPEN_WEATHER_MAP_API_KEY}&q=st.%20louis`
         )
         .then((response) => {
           state.Home.weather = {};
